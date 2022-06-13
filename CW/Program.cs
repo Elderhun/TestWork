01274a683886e001ce7b5a31652cc0e07622257d
Elderhun <kiselovigor970@gmail.comm>
2022-06-13 12:43:28 +0300

    Написал первый этап программы. Сделал описание в Readme

--- CW/Program.cs
+++ CW/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] StringArr()
+{
+    string resource = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*-+<=+";
+    Random rnd = new Random();
+    int position = 0;
+    string[] strMass = new string[rnd.Next(3,9)];
+
+    for (int i = 0; i < strMass.Length; i++)
+    {
+       string strEmp = string.Empty;
+
+        for(int j = 0; j < rnd.Next(1,9); j++)
+        {
+
+        position = rnd.Next(0, resource.Length);
+        strEmp += resource[position];
+
+        }   
+
+        strMass[i] = strEmp;
+
+    }
+
+return strMass;
+}
